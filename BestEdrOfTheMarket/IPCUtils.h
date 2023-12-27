77de8d3d8245859c3d55046bfbc40332a1ed337e
Xacone <benjamaayazid21@gmail.com>
2023-12-27 02:45:20 +0100

    Address data receiving through IPC + Address conversion & analysis

--- BestEdrOfTheMarket/IPCUtils.h
+++ BestEdrOfTheMarket/IPCUtils.h
@@ -4,11 +4,12 @@
 #include <tchar.h>
 #include <strsafe.h>
 #include <iostream>
+#include <vector>
+#include <json/json.h> 
+
 #include "ErrorsReportingUtils.h"
 #include "ColorsUtils.h"
 #include "BytesSequencesUtils.h"
-#include <vector>
-#include <json/json.h> 
 
 #define PIPE_BUFFER_SIZE 512
 
@@ -63,7 +64,7 @@ public:
 	HANDLE initPipeAndWaitForConnection() {
 
 		HANDLE hPipe;
-		char buffer[4096];
+		char buffer[256];
 		DWORD bytesRead;
 
 		while (true) {
@@ -97,19 +98,80 @@ public:
 					std::istringstream jsonStream(jsonString);
 
 					if (Json::parseFromStream(reader, jsonStream, &root, nullptr)) {
+						
 						std::string routineName = root["Function"].asString();
 						
 						printBlueAlert("Intercepted " + routineName);	
 
 						size_t capturedDataSize = (size_t)strlen(root["Data"].asCString());
-						BYTE* hexDump = hexStringToBytes(root["Data"].asCString(), capturedDataSize);
+						BYTE* addr = hexStringToBytes(root["Data"].asCString(), capturedDataSize);
 
+						//BYTE sAddr[8] = { addr[0], addr[1], addr[2], addr[3], addr[4], addr[5], addr[6], addr[7] };
+						BYTE rAddr[8] = { addr[7], addr[6], addr[5], addr[4], addr[3], addr[2], addr[1], addr[0] };
+
+						LPCVOID addrPointer;
+						memccpy(&addrPointer, addr, 8, 8);
+
+						BYTE dump[1024];
+						size_t dumpBytesRead;
 
 						///TODO : debug
-						std::string jsonDump((char*)buffer);
-						std::cout << jsonDump << "\n" << std::endl;
+						if (_v_) {
+							std::string jsonDump(jsonString);
+							std::cout << jsonDump << "\n" << std::endl;
+							printf("Received address: 0x%02X%02X%02X%02X%02X%02X%02X%02X\n",
+								rAddr[0], rAddr[1], rAddr[2], rAddr[3], rAddr[4], rAddr[5], rAddr[6], rAddr[7]);
+
+							printByteArray(dump, dumpBytesRead);
+						}
+
+						if (ReadProcessMemory(targetProcess, (LPCVOID)addrPointer, dump, sizeof(dump), &dumpBytesRead)) {
+							
+							for (const auto& pair : dllPatterns) {
+								if (containsSequence(dump, dumpBytesRead, pair.first, pair.second)) {
+									alertAndKillThatProcess(targetProcess);
+									deleteMonitoringFunc();
+									startupFunc();
+								}
+							}
+
+							/*
+							for (const auto& pair : generalPatterns) {
+								if (containsSequence(dump, dumpBytesRead, pair.first, pair.second)) {
+									alertAndKillThatProcess(targetProcess);
+									deleteMonitoringFunc();
+									startupFunc();
+								}
+							}*/
+							
+						}
+
 						
 
+
+						//DWORD_PTR dwordPtrValue = *reinterpret_cast<DWORD_PTR*>(rAddr);
+						//LPVOID lpvoidValue = reinterpret_cast<LPVOID>(dwor dPtrValue);
+
+						//std::cout << std::hex << (DWORD_PTR)dwordPtrValue << std::endl;
+						//std::cout << std::hex << (LPVOID)dwordPtrValue << std::endl;
+
+						/*
+						BYTE dump[2048];
+						size_t dumpBytesRead;
+
+						reverseBytesOrder(addr, capturedDataSize);
+
+						if (ReadProcessMemory(targetProcess, pVoidPointer, dump, sizeof(dump), &dumpBytesRead)) {
+							printByteArray(dump, dumpBytesRead);
+						} else {
+							std::cout << "bah non chef " << std::endl;
+						}
+						*/
+
+
+						
+
+						/*
 						for (const auto& pair : dllPatterns) {
 							if (containsSequence(hexDump, bytesRead, pair.first, pair.second)) {
 								alertAndKillThatProcess(targetProcess);
@@ -125,14 +187,10 @@ public:
 								startupFunc();
 							}
 						}
-
+						*/
+						
 					}
-
-
 				}
-
-			
- 
 			}
 		}
 
