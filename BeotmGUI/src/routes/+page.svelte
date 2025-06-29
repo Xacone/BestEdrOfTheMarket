18621268872e06d4c7eb634fca51ca9b00d1f5d7
LFLCH <leofiloche.wk@gmail.com>
2025-06-30 00:42:20 +0200

    add basic dark mode

--- BeotmGUI/src/routes/+page.svelte
+++ BeotmGUI/src/routes/+page.svelte
@@ -1,6 +1,20 @@
-<nav class="flex items-baseline justify-center gap-2 pt-2">
-	<h1 class="text-4xl font-bold">BEOTM</h1>
-	<span class="text-primary text-2xl">GUI</span>
-</nav>
+<script>
+  import '../app.css';
+  import DarkMode from '$lib/components/DarkMode.svelte';
+  import GitHubLink from '$lib/components/GitHubLink.svelte';
+</script>
 
-Hello, World !
+<div class="p-5">
+  <header class="flex items-baseline justify-between">
+    <div class="flex items-baseline gap-2">
+      <h1 class="text-4xl font-bold">BEOTM</h1>
+      <span class="text-primary text-2xl">GUI</span>
+    </div>
+    <div class="flex items-baseline">
+      <!-- (later) Why not a panel / grid icon to manage the view -->
+      <DarkMode></DarkMode>
+      <div class="divider divider-horizontal"></div>
+      <GitHubLink></GitHubLink>
+    </div>
+  </header>
+</div>
