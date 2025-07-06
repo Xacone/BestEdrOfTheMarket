e19d2e8e23ecc19352a03ddc88c440b10f54219e
LFLCH <leofiloche.wk@gmail.com>
2025-07-06 21:39:43 +0200

    list elements first idea

--- BeotmGUI/src/lib/components/ListDetectionEvents.svelte
+++ BeotmGUI/src/lib/components/ListDetectionEvents.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
+  import LetterText from '@lucide/svelte/icons/letter-text';
+  import Binary from '@lucide/svelte/icons/binary';
+  import ChevronRight from '@lucide/svelte/icons/chevron-right';
+  import ShieldCheck from '@lucide/svelte/icons/shield-check';
+  import SquareFunction from '@lucide/svelte/icons/square-function';
+  import BowArrow from '@lucide/svelte/icons/bow-arrow';
+  import Target from '@lucide/svelte/icons/target';
+
   // TODO: move fetching logic at the page level or as a util
   // Use svelte stores to propagate data between components ?
   // (probably simpler): using child props
@@ -11,6 +19,8 @@
     events.forEach((receivedEvent) => detectedEvents.push(receivedEvent));
     detectedEvents.sort((evt1, evt2) => evt1.DateAndTime.getTime() - evt2.DateAndTime.getTime());
   }, 2000);
+
+  let displayId = $state(false);
 </script>
 
 <!-- <th>Detection Time</th>
@@ -18,21 +28,64 @@
 <th>Global defensive method</th>
 <th>Origin process</th>
 <th>Victim process</th> -->
-<div class="bg-base-300 w-200 flex-1 overflow-hidden rounded-xl pb-10">
-  <div class="bg-base-200 p-2">Detected events</div>
-  <ul class="h-full overflow-y-scroll">
+
+<div class="border-base-300 flex w-125 flex-1 flex-col overflow-hidden rounded-xl border-1">
+  <div class="bg-base-200 p-2">
+    TODO: sort options
+    <button
+      class="btn btn-square"
+      onclick={() => {
+        displayId = !displayId;
+      }}
+    >
+      <label class="swap swap-rotate">
+        <input type="checkbox" bind:checked={displayId} disabled />
+        <LetterText class="swap-off h-7 w-7" strokeWidth="0.7"></LetterText>
+        <Binary class="swap-on h-7 w-7" strokeWidth="0.7"></Binary>
+      </label>
+    </button>
+  </div>
+  <ul class="flex-1 overflow-y-scroll">
     {#each detectedEvents as evt, i}
       {#if i === 0 || evt.DateAndTime.toDateString() !== detectedEvents[i - 1].DateAndTime.toDateString()}
-        <li class="bg-base-300 sticky top-0 z-10 p-2">{evt.DateAndTime.toLocaleDateString()}</li>
+        <li
+          class="bg-base-100 border-b-base-300 sticky top-0 border-b-1 p-0.5 text-right font-semibold"
+        >
+          {evt.DateAndTime.toLocaleDateString()}
+        </li>
       {/if}
-      <li class="flex pr-5 pl-5">
-        {evt.DateAndTime.getHours()}:{evt.DateAndTime.getMinutes()}
-        {evt.Level}
-        {evt.GlobalDefensiveMethod}
-        {evt.OriginProcess}
-        {evt.VictimProcess}
-        {evt.InvolvedYaraRule}
+      <li class="border-b-base-300 hover:bg-base-200 flex w-full cursor-pointer border-b-1 p-2">
+        <div class="w-full">
+          <div class="flex gap-2">
+            <div>{evt.DateAndTime.toLocaleTimeString()}</div>
+            <div class="badge badge-{evt.Level.replace('Critical', 'Error').toLowerCase()}">
+              {evt.Level}
+            </div>
+            <div class="flex">
+              <span class="font-semibold">
+                <!-- <BowArrow class="h-5 w-5" strokeWidth="0.7"></BowArrow> -->
+                {displayId ? evt.OriginPID : evt.OriginProcess}
+              </span>
+              <ChevronRight class="h-7" strokeWidth="1"></ChevronRight>
+              <span>
+                <!-- <Target class="h-5 w-5" strokeWidth="0.7"></Target> -->
+                {displayId ? evt.TargetPID : evt.VictimProcess}
+              </span>
+            </div>
+          </div>
+          <div class="flex flex-col gap-2 pt-1">
+            <div class="flex items-center">
+              <SquareFunction class="h-5 w-5" strokeWidth="0.7"></SquareFunction> :
+              {displayId ? evt.GlobalDefensiveMethodId : evt.GlobalDefensiveMethod}
+            </div>
+            <div class="flex items-center">
+              <ShieldCheck class="h-5 w-5" strokeWidth="0.7"></ShieldCheck> :
+              {evt.InvolvedYaraRule}
+            </div>
+          </div>
+        </div>
       </li>
     {/each}
   </ul>
+  <div class="bg-base-200 p-2">TODO: filter options</div>
 </div>
