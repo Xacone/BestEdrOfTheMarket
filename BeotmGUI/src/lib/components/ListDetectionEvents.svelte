9ed9e69f2954dd72f5209021834b0c6beb34fc5d
Spraduss <louis.ruellou@gmail.com>
2025-07-07 14:31:30 +0200

    Premier jet du détail des alertes

--- BeotmGUI/src/lib/components/ListDetectionEvents.svelte
+++ BeotmGUI/src/lib/components/ListDetectionEvents.svelte
@@ -9,6 +9,9 @@
   // Use svelte stores to propagate data between components ?
   // (probably simpler): using child props
   import { DetectionEventSchema, type DetectionEvent } from '$lib/schemas/detection-events';
+  import DetailsAlerte from '$lib/components/DetailsAlerte.svelte';
+
+  let eventClickedForMoreDetails: DetectionEvent | null = $state(null);
 
   let detectedEvents: DetectionEvent[] = $state([]);
   setInterval(async () => {
@@ -27,7 +30,8 @@
 <th>Origin process</th>
 <th>Victim process</th> -->
 
-<div class="border-base-300 flex w-125 flex-1 flex-col overflow-hidden rounded-xl border-1">
+<div class="flex">
+  <div class="border-base-300 flex w-125 flex-1 flex-col overflow-hidden rounded-xl border-1">
   <div class="bg-base-200 p-2">
     TODO: sort options
     <button
@@ -55,7 +59,11 @@
       <li
         class="border-b-base-300 hover:bg-base-200 flex w-full cursor-pointer border-b-1 p-2 pt-0.5"
       >
-        <div class="w-full">
+        <div class="w-full" 
+          onclick={() => eventClickedForMoreDetails = evt}
+          onkeyup={() => eventClickedForMoreDetails = evt}
+          role="button"
+          tabindex="0">
           <div class="flex items-center gap-2">
             <div class="text-sm">{evt.DateAndTime.toLocaleTimeString()}</div>
             <div
@@ -92,4 +100,7 @@
     {/each}
   </ul>
   <div class="bg-base-200 p-2">TODO: filter options</div>
+</div>
+<DetailsAlerte bind:eventClickedForMoreDetails={eventClickedForMoreDetails}></DetailsAlerte>
+
 </div>
